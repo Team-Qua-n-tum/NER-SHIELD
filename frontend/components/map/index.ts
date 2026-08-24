4369dd3eb12be817ce5efb5182571d77e5694026
Naveen Varadharajan <naveenv24it@srishakthi.ac.in>
2026-08-24 14:19:36 +0530

    chore: clean up Next.js configuration and dependencies

--- frontend/components/map/index.ts
+++
@@ -1,8 +0,0 @@
-export { NERMap } from "./NERMap";
-export { DistrictLayer } from "./DistrictLayer";
-export { RoadLayer } from "./RoadLayer";
-export { IncidentMarkers } from "./IncidentMarkers";
-export { VehicleMarkers } from "./VehicleMarkers";
-export { RiskLayer } from "./RiskLayer";
-export { RouteLayer } from "./RouteLayer";
-export type { NERMapProps } from "./NERMap";
